cfa294731a1770277614b55582615792d283ed18
Assia-ab <assiaabizouhour123@gmail.com>
2024-06-07 18:18:28 +0100

    thousand commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import MonCV from './MonCV';
 import FormPage from './FormPage';
 
